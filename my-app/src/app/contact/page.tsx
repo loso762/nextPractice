ed30d0a6ebc7988ef7b9c20d6b8617fbe05b0138
loso762 <loso762@naver.com>
2023-08-03 21:58:29 +0900

    정적 라우팅 및 동적 라우팅 공부

---
+++ my-app/src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>contact Us!</div>;
+}
+
+export default page;
---
+++ my-app/src/app/products/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function productspage() {
+  return <div>products!</div>;
+}
+
+export default productspage;
