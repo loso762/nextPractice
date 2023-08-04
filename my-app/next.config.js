b43c72958cfd46d83b5f9cf2cd9646c82466e4cd
loso762 <loso762@naver.com>
2023-08-04 16:13:33 +0900

    next.config.js 파일에 redirects 속성을 통해 잘못된 주소로 이동하면 redirect 해주기

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const {factory} = require("typescript");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/pants/delete_forever",
+        destination: "/pants",
+        permanent: true,
+      },
+      {
+        source: "/pants/delete_temp",
+        destination: "/pants",
+        permanent: false,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- my-app/src/app/(products)/pants/page.tsx
+++ my-app/src/app/(products)/pants/page.tsx
@@ -1,14 +1,17 @@
 import {getProducts} from "@/service/products";
 import Link from "next/link";
 import React from "react";
+import clothesImg from "../../../../public/images/1.jpeg";
+import Image from "next/image";
 
 async function Pantspage() {
-  throw new Error();
+  //throw new Error();
   const products = await getProducts();
 
   return (
     <>
-      <h1>제품소개 페이지</h1>
+      <h1>제품소개 페이지!</h1>
+      <Image src={clothesImg} alt="clothes" width={400} height={400} />
       <ul>
         {products.map((product, idx) => (
           <li key={idx}>
